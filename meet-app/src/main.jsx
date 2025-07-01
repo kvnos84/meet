a0f74982143f6eb346ad183ccf607cdf63656068
kvnos84 <kevin.osdesign@gmail.com>
2025-07-01 14:22:15 +0200

    atatus

--- meet-app/src/main.jsx
+++ meet-app/src/main.jsx
@@ -1,3 +1,6 @@
+import * as atatus from 'atatus-spa';
+atatus.config('25de2f8822144301983e8129314ac423').install();
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
